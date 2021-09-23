ea8447abcf26076223e5070eb6c2777077842f5c
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-09-23 23:21:01 +0530

    Delete gofile2 directory

--- gofile2/__init__.py
+++
@@ -1,6 +0,0 @@
-# Original Author: Codec04
-# Re-built by Itz-fork
-# Project: Gofile2
-
-from .gofile_core.async_gofile2 import Async_Gofile
-from .gofile_core.gofile2 import Gofile
--- gofile2/gofile_core/__init__.py
+++
@@ -1,5 +0,0 @@
-# Original Author: Codec04
-# Re-built by Itz-fork
-# Project: Gofile2
-
-__version__ = "v1.3.1"
